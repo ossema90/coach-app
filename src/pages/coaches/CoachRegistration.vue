ffb49a979e1f6ee28c8dfc3812c5fc6e4a48e1fb
ossema90 <d.ossema.daoud@gmail.com>
2023-06-01 19:57:04 +0100

    project deployment

---
+++ src/pages/coaches/CoachRegistration.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+
+        <base-card>
+            <section>
+                <h2>Register as a coach now! </h2>
+            </section>
+            <coach-form @save-data="saveData"></coach-form>
+        </base-card>
+    </div>
+</template>
+<script>
+import CoachForm from '../../components/coaches/CoachForm.vue';
+export default {
+    components: {
+        CoachForm
+    },
+    methods: {
+        saveData(data) {
+            this.$store.dispatch('coaches/registerCoach', data);
+            this.$router.replace('/coaches');
+        }
+    }
+}
+</script>
