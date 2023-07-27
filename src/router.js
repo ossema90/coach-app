2f67f2af7a2c4242752d1fd5b3dbb4fe0d2c5b1d
ossema90 <d.ossema.daoud@gmail.com>
2023-07-27 14:20:18 +0100

    update

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from 'vue-router';
 
 import CoachDetail from './pages/coaches/CoachDetail.vue';
 import CoachesList from './pages/coaches/CoachesList.vue';
@@ -9,30 +9,40 @@ import UserAuth from './pages/auth/UserAuth.vue';
 import NotFound from './pages/NotFound.vue';
 import store from './store/index.js';
 
-const router= createRouter({
-
-    history : createWebHashHistory(),
-    routes: [
-        {path :'/', redirect : '/coaches'},
-        {path : '/coaches' , component : CoachesList},
-        {path : '/coaches/:id' , component : CoachDetail ,props:true, children : [
-            {path : 'contact' , component : ContactCoach}  //coaches/c1/contact
-        ]},
-        {path : '/register' , component : CoachRegistration , meta : {requiresAuth : true}},
-        {path : '/requests' , component : RequestsReceived , meta : {requiresAuth : true}},
-        {path:'/auth' , component:UserAuth , meta : {requiresUnauth : true}},
-        {path : '/:notFound(.*)' , component : NotFound},
-    ]
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes: [
+    { path: '/', redirect: '/coaches' },
+    { path: '/coaches', component: CoachesList },
+    {
+      path: '/coaches/:id',
+      component: CoachDetail,
+      props: true,
+      children: [
+        { path: 'contact', component: ContactCoach }, //coaches/c1/contact
+      ],
+    },
+    {
+      path: '/register',
+      component: CoachRegistration,
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/requests',
+      component: RequestsReceived,
+      meta: { requiresAuth: true },
+    },
+    { path: '/auth', component: UserAuth, meta: { requiresUnauth: true } },
+    { path: '/:notFound(.*)', component: NotFound },
+  ],
 });
-router.beforeEach(function(to,from,next)
-{
-    if(to.meta.requiresAuth && !store.getters.isAuthenticated){
-        next('/auth');
-    }
-    else if (to.meta.requiresUnauth && store.getters.isAuthenticated){
-        next('/coaches');
-    }
-    next();
+router.beforeEach(function (to, from, next) {
+  if (to.meta.requiresAuth && !store.getters.isAuthenticated) {
+    next('/auth');
+  } else if (to.meta.requiresUnauth && store.getters.isAuthenticated) {
+    next('/coaches');
+  }
+  next();
 });
 
-export default router ;
+export default router;
